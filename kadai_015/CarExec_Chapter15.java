5adb9d99b99fb70cd925f57c519ceb87051983e5
HatsumiKitahara <136227204+HatsumiKitahara@users.noreply.github.com>
2025-09-30 15:24:22 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -4,10 +4,10 @@ public class CarExec_Chapter15 {
 
 	public static void main(String[] args) {
 		// インスタンス生成
-		Car_Chapter15 speed1 = new Car_Chapter15();
+		Car_Chapter15 speed1 = new Car_Chapter15( 1, 10 );
 		
 		// changeGearメソッドの実行
-		speed1.changeGear(3);
+		speed1.changeGear(4);
 		
 		// runメソッドの実行
 		speed1.run();
