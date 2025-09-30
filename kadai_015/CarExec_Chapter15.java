5720e6b7fe51955508d6f1826885a65acc16b3f6
HatsumiKitahara <136227204+HatsumiKitahara@users.noreply.github.com>
2025-09-30 13:51:03 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,17 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// インスタンス生成
+		Car_Chapter15 speed1 = new Car_Chapter15(0, 0);
+		
+		// changeGearメソッドの実行
+		speed1.changeGear(2);
+		
+		// runメソッドの実行
+		speed1.run();
+		
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,34 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	
+	private int gear = 1; // 1速から5速のギアを表す
+	private int speed = 10; // ギアチェンジ後の速度を表す
+	
+	public Car_Chapter15 ( int gear, int speed ) {
+		this.gear= gear;
+		this.speed = speed;
+	}
+	
+	// ギアの値により速度を変える
+	public void changeGear( final int afterGear ) {
+		int afterSpeed = 0;
+		
+		afterSpeed = switch (afterGear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;		
+		};
+		this.speed = afterSpeed;
+		this.gear = afterGear;
+	}
+	
+	// ギアチェンジ後の速度を表示する
+	public void run() {
+		System.out.println("ギア" + this.gear + "の速度は" + this.speed + "です");
+		
+	}
+}
